d1c6b527b8e668b7cb817e04bdcdcde953c93ab9
Taha Siraj <160783918+Tahasiraj1@users.noreply.github.com>
2024-09-24 16:11:50 +0500

    Update pomodoro-timer.tsx

--- components/pomodoro-timer.tsx
+++ components/pomodoro-timer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useRef, useMemo, useCallback } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { Button } from "@/components/ui/button";
 import { 
     MinusIcon, 
